d349f20238924ea192b0cc9c18a6689f6d85c430
Edwin Lunando <edwinlunando@gmail.com>
2015-09-14 10:59:53 +0700

    fix assets precompile

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -4,5 +4,5 @@ Chef::Log.info('Pecompile Assets')
 execute 'rake assets:precompile' do
   cwd release_path
   command "bundle exec rake assets:precompile"
-  environment 'RAILS_ENV' => new_resource.environment['RAILS_ENV'], 'SECRET_KEY_BASE' => new_resource.environment['SECRET_KEY_BASE']
+  environment 'RAILS_ENV' => new_resource.environment['RAILS_ENV'], 'SECRET_KEY_BASE' => 'ab1', 'DEVISE_SECRET_KEY' => 'abc1'
 end
