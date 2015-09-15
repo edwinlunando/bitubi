77a9ead465e4d34c3f0f8419a326e0e7c453cb93
Edwin Lunando <edwinlunando@gmail.com>
2015-09-15 13:55:00 +0700

    add clear cache before restart

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -4,10 +4,27 @@ Chef::Log.info('Pecompile Assets')
 execute 'rake assets:precompile' do
   cwd release_path
   command "bundle exec rake assets:precompile"
+
   environment 'RAILS_ENV' => new_resource.environment['RAILS_ENV'],
               'SECRET_KEY_BASE' => new_resource.environment['SECRET_KEY_BASE'],
               'DEVISE_SECRET_KEY' => new_resource.environment['SECRET_KEY_BASE'],
               'FOG_DIRECTORY' => new_resource.environment['FOG_DIRECTORY'],
               'AWS_ACCESS_KEY_ID' => new_resource.environment['AWS_ACCESS_KEY_ID'],
               'AWS_SECRET_ACCESS_KEY' => new_resource.environment['AWS_SECRET_ACCESS_KEY']
+
 end
+
+Chef::Log.info('Clear cache')
+execute 'rake tmp:cache:clear' do
+  cwd release_path
+  command "rake tmp:cache:clear"
+
+  environment 'RAILS_ENV' => new_resource.environment['RAILS_ENV'],
+              'SECRET_KEY_BASE' => new_resource.environment['SECRET_KEY_BASE'],
+              'DEVISE_SECRET_KEY' => new_resource.environment['SECRET_KEY_BASE'],
+              'FOG_DIRECTORY' => new_resource.environment['FOG_DIRECTORY'],
+              'AWS_ACCESS_KEY_ID' => new_resource.environment['AWS_ACCESS_KEY_ID'],
+              'AWS_SECRET_ACCESS_KEY' => new_resource.environment['AWS_SECRET_ACCESS_KEY']
+
+end
+
