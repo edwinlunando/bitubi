8371493d435fd37c8f101f94e46d82506f775b0e
Edwin Lunando <edwinlunando@gmail.com>
2015-11-09 14:06:24 +0700

    remove lograge

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,11 +94,11 @@ Rails.application.configure do
   }
 
   # lograge
-  config.lograge.enabled = true
-  config.lograge.custom_options = lambda do |event|
-    options = event.payload.slice(:request_id, :user_id, :visit_id)
-    options[:params] = event.payload[:params].except('controller', 'action')
-    options
-  end
+  # config.lograge.enabled = true
+  # config.lograge.custom_options = lambda do |event|
+  #   options = event.payload.slice(:request_id, :user_id, :visit_id)
+  #   options[:params] = event.payload[:params].except('controller', 'action')
+  #   options
+  # end
 
 end
