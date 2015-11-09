234c9a61cbfd65cdc05400b4218719267288a4de
Edwin Lunando <edwinlunando@gmail.com>
2015-11-09 19:11:27 +0700

    precompile

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,18 +1,18 @@
 Chef::Log.info('Running deploy/before_migrate.rb...')
 
-# Chef::Log.info('Pecompile Assets')
-# execute 'rake assets:precompile' do
-#   cwd release_path
-#   command "bundle exec rake assets:precompile"
+Chef::Log.info('Pecompile Assets')
+execute 'rake assets:precompile' do
+  cwd release_path
+  command "bundle exec rake assets:precompile"
 
-#   environment 'RAILS_ENV' => new_resource.environment['RAILS_ENV'],
-#               'SECRET_KEY_BASE' => new_resource.environment['SECRET_KEY_BASE'],
-#               'DEVISE_SECRET_KEY' => new_resource.environment['SECRET_KEY_BASE'],
-#               'FOG_DIRECTORY' => new_resource.environment['FOG_DIRECTORY'],
-#               'AWS_ACCESS_KEY_ID' => new_resource.environment['AWS_ACCESS_KEY_ID'],
-#               'AWS_SECRET_ACCESS_KEY' => new_resource.environment['AWS_SECRET_ACCESS_KEY']
+  environment 'RAILS_ENV' => new_resource.environment['RAILS_ENV'],
+              'SECRET_KEY_BASE' => new_resource.environment['SECRET_KEY_BASE'],
+              'DEVISE_SECRET_KEY' => new_resource.environment['SECRET_KEY_BASE'],
+              'FOG_DIRECTORY' => new_resource.environment['FOG_DIRECTORY'],
+              'AWS_ACCESS_KEY_ID' => new_resource.environment['AWS_ACCESS_KEY_ID'],
+              'AWS_SECRET_ACCESS_KEY' => new_resource.environment['AWS_SECRET_ACCESS_KEY']
 
-# end
+end
 
 Chef::Log.info('Remove Cache')
 execute 'sudo rm -rf tmp/cache' do
