5820fb7730ac28018221e684dba229764b5c8155
Edwin Lunando <edwinlunando@gmail.com>
2015-11-16 17:28:49 +0700

    fix image regex

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -23,6 +23,6 @@ class Supplier < ActiveRecord::Base
     medium: '300x300>',
     thumb: '100x100>'
   }
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates_attachment_content_type :image, content_type: ['image/jpeg', 'image/gif', 'image/png']
 
 end
