4b57b5153af3f19aac40a2670453439a4f250ce8
Georgius Rinaldo <dodoxzs@gmail.com>
2015-12-04 14:48:54 +0700

    topup reposition, vendorcards, and add equal height js

--- app/assets/javascripts/bitubi.js
+++ app/assets/javascripts/bitubi.js
@@ -203,6 +203,41 @@
                 $('.page-notification--close-btn').click(function() {
                     $('.page-notification--container').fadeToggle('fast');
                 });
+            },
+
+            this.equalHeight = function() {
+                var currentTallest = 0,
+                    currentRowStart = 0,
+                    rowDivs = new Array(),
+                    $el,
+                    topPosition = 0;
+
+                $('.cards').each(function() {
+                    $el = $(this);
+                    topPosition = $el.position().top;
+
+                    if (currentRowStart != topPosition) {
+                        // we just came to a new row.  Set all the heights on the completed row
+                        for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+                            rowDivs[currentDiv].height(currentTallest);
+                        }
+                        // set the variables for the new row
+                        rowDivs.length = 0; // empty the array
+                        currentRowStart = topPosition;
+                        currentTallest = $el.height();
+                        rowDivs.push($el);
+
+                    } else {
+                    // another div on the current row.  Add it to the list and check if it's taller
+                    rowDivs.push($el);
+                    currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+                    }
+
+                    // do the last row
+                    for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+                        rowDivs[currentDiv].height(currentTallest);
+                    }
+                });
             }
         },
 
@@ -286,6 +321,7 @@
                     ui.orderTableInit();
                     ui.notifCloseInit();
                     ui.saldoInit();
+                    ui.equalHeight();
                     // Site.organicTabs("#surfari-tabs");
                 }
             })
