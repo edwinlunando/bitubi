e9c8bb05af305a03e8ea23471bcb7783d747174e
Edwin Lunando <edwinlunando@gmail.com>
2016-02-04 11:00:43 +0700

    add delivery state to admin

--- app/models/order.rb
+++ app/models/order.rb
@@ -41,7 +41,7 @@ class Order < ActiveRecord::Base
   # Method
 
   def self.admin
-    where(state: [:payment, :delivery, :done, :failed])
+    where(state: [:delivery, :payment, :delivery, :done, :failed])
   end
 
   def finish_order
