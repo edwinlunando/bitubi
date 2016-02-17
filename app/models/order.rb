5e170bb50941a5bfd89da55524b3779196bee289
Georgius Rinaldo <dodoxzs@hotmail.com>
2016-02-17 12:04:08 +0700

    add filter and datepicker (html5)

--- app/models/order.rb
+++ app/models/order.rb
@@ -34,6 +34,8 @@ class Order < ActiveRecord::Base
   belongs_to :user
   belongs_to :state_shipment_price
 
+  attr_accessor :date_start, :date_end
+
   # scope
   scope :vendor, -> { where(state: [:delivery, :done, :failed]) }
   scope :created, -> { order(created_at: :desc) }
