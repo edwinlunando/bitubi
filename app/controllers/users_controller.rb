9a187ae6ba131468d5534639e8541392849d2971
Edwin Lunando <edwinlunando@gmail.com>
2016-02-27 12:40:42 +0700

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -234,7 +234,7 @@ class UsersController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:name, :slug, :stock, :description, :category_id, :unit, :weight, :price_dropship, :recommended_price,
+    params.require(:product).permit(:name, :slug, :stock, :description, :category_id, :unit, :weight, :price_dropship, :recommended_price, :published,
                                     wholesale_prices_attributes: [:id, :price, :minimum_quantity, :maximum_quantity, :_destroy],
                                     product_images_attributes: [:id, :data, :_destroy])
   end
