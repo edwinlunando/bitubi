64c98eeb04ab8667041a62c0b031b4ad48cdd4f3
Edwin Lunando <edwinlunando@gmail.com>
2016-07-23 14:28:29 +0700

    change add to append

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,5 +8,5 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += 'tinymce/plugins/*'
-Rails.application.config.assets.precompile += 'tinymce/skins/*'
+Rails.application.config.assets.precompile << 'tinymce/plugins/*'
+Rails.application.config.assets.precompile << 'tinymce/skins/*'
